bb2967807ffc1321f3d0d29f22273d2b0da532b9
alex g <alexg@getcodified.com>
2022-02-16 14:43:53 +0100

    changes in minikube script version

--- install_kubectl_minikube.sh
+++ install_kubectl_minikube.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
-
-#installing kubectl on your vm, a specific version
-echo 'installing kubectl on your vm, a specific version'
-curl -LO https://dl.k8s.io/release/v1.23.0/bin/linux/amd64/kubectl
+#first let's check your linux version just in case:
+echo 'your linux version is:'
+uname --all
+#then we will step on specific minikube version
+version=v1.24.0
+#downloading minikube on your vm, a specific version
+echo 'dowloading minikube on your vm, a specific version:'
+curl -LO https://storage.googleapis.com/minikube/releases/$version/minikube-linux-amd64
 #installing minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/1.24/minikube-linux-amd64 \
-  && chmod +x minikube
+echo 'installing minikube'
+install minikube-linux-amd64 /usr/local/bin/minikube
+#finally, we will check minikube version
+echo 'your minikube version:'
+minikube version
