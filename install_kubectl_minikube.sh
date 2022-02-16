f8a1ec6c22beeffc6e866d515fefad01b16f15a7
alex g <alexg@getcodified.com>
2022-02-16 22:12:18 +0100

    final_add_for_kubectl

--- install_kubectl_minikube.sh
+++ install_kubectl_minikube.sh
@@ -26,3 +26,18 @@ sudo usermod -aG docker $USER && newgrp docker
 #add additinal requirements
 echo 'fixing error with start'
 sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube
+
+#additonally installing connectivity tools for running integraton tests in minikube
+echo 'installing conntrack:'
+sudo apt-get update -y && sudo apt-get install -y conntrack && conntrack --version
+
+#starting minikube
+minikube start
+
+#installing kubectl, first define version
+echo 'installing kubectl'
+versionkubectl=v1.23.0
+#dowloading kubectl
+curl -LO https://dl.k8s.io/release/$versionkubectl/bin/linux/amd64/kubectl
+#installing kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
