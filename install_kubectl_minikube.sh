ebd62b8fc9fd4e29ca127a9d3c7208b21bf0d241
alex g <alexg@getcodified.com>
2022-03-03 17:45:17 +0100

    issue correct minikube installation script by Oleg

--- install_kubectl_minikube.sh
+++ install_kubectl_minikube.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 #first, let's install needed things for startup, for example, docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-#add user to docker group
-sudo usermod -aG docker studentk8s
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 #first let's check your linux version just in case:
 echo 'your linux version is:'
 uname --all
@@ -11,33 +9,31 @@ uname --all
 version=v1.24.0
 #downloading minikube on your vm, a specific version
 echo 'dowloading minikube on your vm, a specific version:'
-curl -LO https://storage.googleapis.com/minikube/releases/$version/minikube-linux-amd64
+sudo curl -LO https://storage.googleapis.com/minikube/releases/$version/minikube-linux-amd64
 #installing minikube
 echo 'installing minikube'
-install minikube-linux-amd64 /usr/local/bin/minikube
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
 #finally, we will check minikube version
 echo 'your minikube version:'
 minikube version
-
 #add user to docker group
 echo 'adding studentk8s to docker group'
-sudo usermod -aG docker $USER && newgrp docker
-
+sudo usermod -aG docker $USER && newgrp docker << ANY
 #add additinal requirements
 echo 'fixing error with start'
 sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube
-
 #additonally installing connectivity tools for running integraton tests in minikube
 echo 'installing conntrack:'
 sudo apt-get update -y && sudo apt-get install -y conntrack && conntrack --version
-
 #starting minikube
 minikube start
-
 #installing kubectl, first define version
 echo 'installing kubectl'
 versionkubectl=v1.23.0
 #dowloading kubectl
-curl -LO https://dl.k8s.io/release/$versionkubectl/bin/linux/amd64/kubectl
+sudo curl -LO https://dl.k8s.io/release/$versionkubectl/bin/linux/amd64/kubectl
 #installing kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+ANY
+#relogging as studentk8s 
+su - $USER
